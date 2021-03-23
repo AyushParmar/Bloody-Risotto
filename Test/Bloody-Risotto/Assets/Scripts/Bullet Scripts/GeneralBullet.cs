56a52f0bd897f0d39c6244a5ba6063a10ba4a127
Kronokilla1337 <varunvyas55@gmail.com>
2021-03-23 17:03:42 +0530

    Player detection implemented

---
+++ Test/Bloody-Risotto/Assets/Scripts/Bullet Scripts/GeneralBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralBullet : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public GameObject hitEffect;
+    public int damage = 25;
+
+    [SerializeField] float fireSpeed = 15f;
+    [SerializeField] float fireVariation;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(transform.right.x, 0) * fireSpeed;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision)
+        {
+            HealthManagement enemy = collision.GetComponent<HealthManagement>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+        Destroy(gameObject);
+        GameObject newHitEffect = Instantiate(hitEffect, rigid.position, Quaternion.identity);
+        Destroy(newHitEffect, 0.4f);
+    }
+}
